cdb1e2789d1c3df411d64ee2ba6373cf35d3346f
wei.liu <liuw198606@aliyun.com>
2017-06-27 10:59:05 +0800

    no message

--- src/main/java/com/leadingsoft/liuw/dto/wx/AbstractMessage.java
+++ src/main/java/com/leadingsoft/liuw/dto/wx/AbstractMessage.java
@@ -3,13 +3,15 @@ package com.leadingsoft.liuw.dto.wx;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 /**
  * 模板消息
  * Created by liuw on 2017/6/26.
  */
 @Getter
 @Setter
-public abstract class AbstractMessage {
+public abstract class AbstractMessage implements Serializable {
     private String touser;
 
     private String template_id;
--- src/main/java/com/leadingsoft/liuw/dto/wx/MeetingMessage.java
+++ src/main/java/com/leadingsoft/liuw/dto/wx/MeetingMessage.java
@@ -4,5 +4,6 @@ package com.leadingsoft.liuw.dto.wx;
  * Created by liuw on 2017/6/26.
  */
 public class MeetingMessage extends AbstractMessage {
+    private static final long serialVersionUID = -4822576246640294727L;
     private MeetingMessageData data;
 }
